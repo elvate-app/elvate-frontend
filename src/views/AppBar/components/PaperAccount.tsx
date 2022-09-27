cc03b6403338e47b48083c46a42733da9410c7e7
Louis Aussedat <aussedat.louis@gmail.com>
2022-09-27 21:46:58 +0200

    AppBar: update PaperAccount with pendinf transaction indicator

--- src/views/AppBar/components/PaperAccount.tsx
+++ src/views/AppBar/components/PaperAccount.tsx
@@ -1,17 +1,33 @@
-import { Paper, styled } from "@mui/material";
+import { AccountBalanceWallet, RotateRight } from "@mui/icons-material";
+import { keyframes, Paper, styled } from "@mui/material";
 import { useWeb3React } from "@web3-react/core";
+import { useMemo } from "react";
 import { CopyTooltip, OpenInNewTooltip } from "src/components/Tooltip";
 import { Subtitle2 } from "src/components/Typo";
+import { NETWORK_MATIC_MUMBAI_TESTNET } from "src/constants/chain";
 import { useExplorer } from "src/hooks/useExplorer";
+import { useTransactions } from "src/hooks/useTransactions";
+
+function animation() {
+  return keyframes`
+  0%{transform: rotate(0deg)};
+  50%{transform: rotate(180deg)};
+  100%{transform: rotate(360deg)};
+}`;
+}
+
+const StyledRotateRight = styled(RotateRight)`
+  animation: ${() => animation()} linear 2s infinite;
+`;
 
 const StyledPaper = styled(Paper)`
   background-color: ${(props) => props.theme.palette.divider};
   color: ${(props) => props.theme.palette.text.primary};
   display: flex;
-  flex-direction: row;
   align-items: center;
   padding: ${(props) => props.theme.spacing(1)};
   margin-bottom: ${(props) => props.theme.spacing(1)};
+  cursor: pointer;
 `;
 
 const StyledBox = styled(Subtitle2)`
@@ -23,21 +39,28 @@ const PaperAccount = () => {
   const { account } = useWeb3React();
   const explorer = useExplorer();
   const smallAddress =
-    account?.substring(0, 6) +
+    account?.substring(0, 7) +
     "..." +
-    account?.substring(account.length - 6, account.length);
+    account?.substring(account.length - 7, account.length);
+
+  const transactions = useTransactions();
+  const pendingTransactions = useMemo(
+    () =>
+      Object.keys(transactions[NETWORK_MATIC_MUMBAI_TESTNET]).filter(
+        (hash: string) =>
+          !transactions[NETWORK_MATIC_MUMBAI_TESTNET][hash]?.blockHash
+      ),
+    [transactions]
+  );
 
   return (
     <StyledPaper>
+      {pendingTransactions.length > 0 ? (
+        <StyledRotateRight />
+      ) : (
+        <AccountBalanceWallet />
+      )}
       <StyledBox>{smallAddress}</StyledBox>
-      <CopyTooltip
-        iconProps={{ sx: { marginLeft: 1 } }}
-        value={account ?? ""}
-      />
-      <OpenInNewTooltip
-        iconProps={{ sx: { marginLeft: 1 } }}
-        value={explorer + "address/" + account}
-      />
     </StyledPaper>
   );
 };
