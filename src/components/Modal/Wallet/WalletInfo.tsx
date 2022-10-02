a753c60725afc94fa61b6e156cd1567348823381
Louis Aussedat <aussedat.louis@gmail.com>
2022-10-02 22:32:36 +0200

    Modal/Wallet: improve ui for Info view

--- src/components/Modal/Wallet/WalletInfo.tsx
+++ src/components/Modal/Wallet/WalletInfo.tsx
@@ -2,25 +2,42 @@ import { styled } from "@mui/material";
 import { useWeb3React } from "@web3-react/core";
 import { BigNumber } from "ethers";
 import { useEffect, useState } from "react";
-import { Flex, FlexColumn } from "src/components/Layout/Flex";
+import { FlexCenter, FlexColumn } from "src/components/Layout/Flex";
 import CustomSkeleton from "src/components/Skeleton";
 import { CopyTooltip, OpenInNewTooltip } from "src/components/Tooltip";
 import { AnimatedNumber, Subtitle1, Subtitle2 } from "src/components/Typo";
 import { useExplorer } from "src/hooks/useExplorer";
+import useTokenList from "src/hooks/useToken";
 
 const Root = styled(FlexColumn)`
   padding: ${(props) => props.theme.spacing(2)};
   background-color: ${(props) => props.theme.palette.background.darker};
 `;
 
+const AddressContainer = styled(FlexCenter)`
+  background-color: ${(props) => props.theme.palette.background.dark};
+  padding: ${(props) => props.theme.spacing(2)};
+  margin-top: ${(props) => props.theme.spacing(2)};
+  justify-content: center;
+  alignitems: center;
+  border-radius: 5px;
+`;
+
+const Img = styled("img")`
+  margin-right: ${(props) => props.theme.spacing(1)};
+  height: 1.5em;
+  width: 1.5em;
+`;
+
 const WalletInfo = () => {
   const { account, library } = useWeb3React();
   const explorer = useExplorer();
   const smallAddress =
-    account?.substring(0, 18) +
+    account?.substring(0, 16) +
     "..." +
-    account?.substring(account.length - 18, account.length);
+    account?.substring(account.length - 16, account.length);
   const [balance, setBalance] = useState<BigNumber | undefined>(undefined);
+  const tokens = useTokenList();
 
   useEffect(() => {
     if (!library || !account) return;
@@ -31,7 +48,7 @@ const WalletInfo = () => {
   return (
     <Root>
       <Subtitle2>Address:</Subtitle2>
-      <Flex justifyContent="center" alignItems="center">
+      <AddressContainer>
         <Subtitle1 sx={{ marginTop: 0.5, flex: 1 }}>{smallAddress}</Subtitle1>
         <CopyTooltip
           value={account || ""}
@@ -41,16 +58,19 @@ const WalletInfo = () => {
           value={explorer + "address/" + account}
           iconProps={{ sx: { marginLeft: 1 } }}
         />
-      </Flex>
+      </AddressContainer>
 
       <Subtitle2 sx={{ marginTop: 2 }}>MATIC Balance:</Subtitle2>
-      <Subtitle1 sx={{ marginTop: 0.5 }}>
-        {balance ? (
-          <AnimatedNumber value={balance} />
-        ) : (
-          <CustomSkeleton width={"7em"} height="100%" />
-        )}
-      </Subtitle1>
+      <AddressContainer>
+        <Img src={tokens[0].icon} />
+        <Subtitle1>
+          {balance ? (
+            <AnimatedNumber value={balance} />
+          ) : (
+            <CustomSkeleton width={"5em"} height="1em" />
+          )}{" "}
+        </Subtitle1>
+      </AddressContainer>
     </Root>
   );
 };
